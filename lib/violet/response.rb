7348d2a2462a97e1d07508cabec3a9045f78fd79
Perrin Alexandre <makoto.kaworu@gmail.com>
2007-11-27 08:53:27 +0100

    removed license from rb files

--- lib/violet/response.rb
+++ lib/violet/response.rb
@@ -1,29 +1,6 @@
-# lib/violet/response.rb
-#
-# Copyright (c) 2007 Perrin Alexandre
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-
-
 =begin rdoc
-==response.rb
+
+==violet/response.rb
 
 
 this module handle servers messages.
@@ -173,12 +150,12 @@ module Response
 
 
   # superclass of messages with no infos.
-  # TODO:
   class EmptyServerRsp < Base::ServerRsp; end
 
 
   #
   # Errors messages from server
+  #
   # TODO: complete doc => request references
   #
 
@@ -407,7 +384,6 @@ module Response
       klass = Helpers.constantize "#{self}::#{Response.constants.grep(klassname).first}"
       raise if klass.nil?
     rescue
-      $! = nil
       raise ProtocolExcepion.new("unknown server's response : #{raw}")
     end
 
